7486cb418dab4138ef6c0996728467e2d58d9dcf
Ankit Soni <ankitsoni9@gmail.com>
2012-11-03 09:30:53 -0400

    Last ditch effort lol

--- chromeextension/annotate.js
+++ chromeextension/annotate.js
@@ -1,35 +1,6 @@
 var isMainDude = false;
-if ("WebSocket" in window)
-{
-  var channel = 1;
-  //var socket = new WebSocket("ws://ec2-50-112-8-217.us-west-2.compute.amazonaws.com/channel/" + channel); 
-  var socket = new WebSocket("ws://128.237.202.85:9000/channel/"+channel);
-  socket.onopen = function(){  
-    console.log('socket open\n');
-  } 
-
-  socket.onmessage = function(htmlfileEvt){  
-    if(!isMainDude) {
-        //change current page to it 
-        newPageSource = htmlfileEvt.data;
-        var newDoc = document.open("text/html", "replace");
-        newDoc.write(newPageSource);
-        newDoc.close();
-    }
-  }
-
-  socket.onclose = function(){  
-    alert("connection closed");
-  }
-}
-else {
-  alert("WebSocket NOT supported by your Browser!");
-}
-
-
-var lastID = null;
-var startID = 0;
-
+var lastID = null; //Last visited element id
+var startID = 0; //Start id suffix to give ids to elements without ids
 var handleMouseover = function (e) {
     var targetElement = e.target;
     $(targetElement).addClass('highlight');
@@ -55,7 +26,7 @@ var handleMouseClick = function (e) {
     //var pageSource = document.documentElement.outerHTML;
     //socket.send(pageSource);  
 }
-$(document).ready(function(){
+var docReadyHandler = function() { 
   //$('body').append('<div id="container"></div>');
   $('body').append('<div id="annotate_tpl" class="annotate"><textarea class="postit"></textarea><a class="annotate-close"><strong style="color:#ff0000;">x</strong> delete</a></div>');
 
@@ -83,7 +54,7 @@ $(document).ready(function(){
       $(document).on('click', handleMouseClick);
       
       isMainDude = true;
-
+      console.log("bla");
       $('#annotate-overlay-layer').addClass('overlay-layer');
     } 
     else{
@@ -94,4 +65,36 @@ $(document).ready(function(){
       $('#annotate-overlay-layer').removeClass('overlay-layer');
     }
   });
-});
+} 
+if ("WebSocket" in window)
+{
+  var channel = 1;
+  //var socket = new WebSocket("ws://ec2-50-112-8-217.us-west-2.compute.amazonaws.com/channel/" + channel); 
+  var socket = new WebSocket("ws://128.237.202.85:9000/channel/"+channel);
+  socket.onopen = function(){  
+    console.log('socket open\n');
+  } 
+
+  socket.onmessage = function(htmlfileEvt){  
+    if(!isMainDude) {
+        //change current page to it 
+        newPageSource = htmlfileEvt.data;
+        var newDoc = document.open("text/html", "replace");
+        newDoc.write(newPageSource);
+        newDoc.close();
+        $(document).ready(docReadyHandler);
+        alert("lol");
+
+    }
+  }
+
+  socket.onclose = function(){  
+    alert("connection closed");
+  }
+}
+else {
+  alert("WebSocket NOT supported by your Browser!");
+}
+
+
+$(document).ready(docReadyHandler);
