372b684a81fb961a57c7023dce3979796428a9c2
Ankit Soni <ankitsoni9@gmail.com>
2012-11-02 20:30:10 -0400

    added jquery-annotate lib

--- chromeextension/contentscript.js
+++ chromeextension/contentscript.js
@@ -3,11 +3,11 @@ var port = chrome.extension.connect({name: "knockknock"});
 port.postMessage({joke: "Knock knock"});
 
 port.onMessage.addListener(function(msg) {
-	if (msg.question == "Who's there?")
-		port.postMessage({answer: "Madame"});
-	else if (msg.question == "Madame who?")
-		port.postMessage({answer: "Madame... Bovary"});
-	else if (msg.question == "I don't get it.")
-		alert("lol");
+  if (msg.question == "Who's there?")
+    port.postMessage({answer: "Madame"});
+  else if (msg.question == "Madame who?")
+    port.postMessage({answer: "Madame... Bovary"});
+  else if (msg.question == "I don't get it.")
+    alert("lol");
 });
 
