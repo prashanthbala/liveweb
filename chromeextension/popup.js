0122f6aae62b8e5b933a069a0da2824758871e71
Prashanth Balasubramaniam <prashanth.bala9@gmail.com>
2012-11-10 17:43:11 -0500

    removed client folder, chat client added for popup

--- chromeextension/popup.js
+++ chromeextension/popup.js
@@ -23,3 +23,5 @@ else {
                         }
                     });
 }
+
+<script id="sid0010000012958362696">(function() {function async_load(){s.id="cid0010000012958362696";s.src='http://st.chatango.com/js/gz/emb.js';s.style.cssText="width:250px;height:360px;";s.async=true;s.text='{"handle":"livewebchatongo","styles":{"b":60,"f":50,"l":"999999","q":"999999","r":100,"s":1}}';var ss = document.getElementsByTagName('script');for (var i=0, l=ss.length; i < l; i++){if (ss[i].id=='sid0010000012958362696'){ss[i].id +='_';ss[i].parentNode.insertBefore(s, ss[i]);break;}}}var s=document.createElement('script');if (s.async==undefined){if (window.addEventListener) {addEventListener('load',async_load,false);}else if (window.attachEvent) {attachEvent('onload',async_load);}}else {async_load();}})();</script>
